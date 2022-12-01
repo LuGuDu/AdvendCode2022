7e600ff9b1700458db2ad0bdd02305042d351a5c
Lucas Gutierrez <lugudu999@gmail.com>
2022-12-01 09:53:39 +0100

    solved day1

--- Solves/day1/day1.py
+++ Solves/day1/day1.py
@@ -1,14 +1,43 @@
 f = open("input.txt", "r")
 
+#PART 1
 sum = 0
 sumMayor = 0
 sumActual = 0
 for i in f.readlines():
-    i =+ sum
-    if i == "":
+    if i != "\n":
+        sum = sum + int(i)
+    else:
         sumActual = sum
-        if sumActual < sumMayor:
+        if sumActual > sumMayor:
             sumMayor = sumActual
-
+        sum = 0
 print(sumMayor)
 
+
+f = open("input.txt", "r")
+
+#PART 2
+sum = 0
+sumMayor = 0
+sumMayor2 = 0
+sumMayor3 = 0
+sumActual = 0
+for i in f.readlines():
+    if i != "\n":
+        sum = sum + int(i)
+    else:
+        sumActual = sum
+        #print(sumMayor, " - ", sumMayor2, " - ", sumMayor3)
+        sumAuxiliar = 0
+        if sumActual > sumMayor:
+            sumMayor3 = sumMayor2
+            sumMayor2 = sumMayor
+            sumMayor = sumActual
+        elif sumActual > sumMayor2:
+            sumMayor3 = sumMayor2
+            sumMayor2 = sumActual
+        elif sumActual > sumMayor3:
+            sumMayor3 = sumActual
+        sum = 0
+print(sumMayor + sumMayor2 + sumMayor3)
